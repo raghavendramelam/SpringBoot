f305c4698f80896f43324c09117ee84fe8df09e0
raghavendramelam <raghavendramelam@localhost>
2024-10-03 03:09:26 -0500

    removed unused imports

--- BootProj03_RealTimeDI/src/main/java/com/raghava/BootProj03RealTimeDiApplicationTest.java
+++ BootProj03_RealTimeDI/src/main/java/com/raghava/BootProj03RealTimeDiApplicationTest.java
@@ -2,8 +2,6 @@ package com.raghava;
 
 import java.sql.SQLException;
 import java.util.List;
-import java.util.Scanner;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
