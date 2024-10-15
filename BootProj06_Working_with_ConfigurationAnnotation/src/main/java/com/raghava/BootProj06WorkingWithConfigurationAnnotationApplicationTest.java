eeed7cb468874bbea2fc0d1163c87484685e2a35
raghavendramelam <raghavendramelam@localhost>
2024-10-15 04:34:40 -0500

    SpringBoot Application on working with Configuration Annotation

---
+++ BootProj06_Working_with_ConfigurationAnnotation/src/main/java/com/raghava/BootProj06WorkingWithConfigurationAnnotationApplicationTest.java
@@ -0,0 +1,22 @@
+package com.raghava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import com.raghava.sbeans.Hospital;
+
+@SpringBootApplication
+public class BootProj06WorkingWithConfigurationAnnotationApplicationTest {
+
+	public static void main(String[] args) {
+		
+	ApplicationContext ctx = 	SpringApplication.run(BootProj06WorkingWithConfigurationAnnotationApplicationTest.class, args);
+	
+Hospital hsptl2=	ctx.getBean("hsptl",Hospital.class);
+System.out.println(hsptl2);
+((ConfigurableApplicationContext) ctx).close();
+	}
+
+}
---
+++ BootProj06_Working_with_ConfigurationAnnotation/src/main/java/com/raghava/sbeans/Employee.java
@@ -0,0 +1,13 @@
+package com.raghava.sbeans;
+
+import lombok.Data;
+
+@Data
+public class Employee {
+
+	
+	private int empid;
+	private String empname;
+	private String empdesg;
+	private String location;
+}
