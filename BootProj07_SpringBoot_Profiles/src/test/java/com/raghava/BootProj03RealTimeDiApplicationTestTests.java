da96b27b1c667dc6911ff5b45c1237a2d58e7103
raghavendramelam <raghavendramelam@localhost>
2024-10-17 15:58:29 -0500

    Profiles in SpringBoot

---
+++ BootProj07_SpringBoot_Profiles/src/test/java/com/raghava/BootProj03RealTimeDiApplicationTestTests.java
@@ -0,0 +1,13 @@
+package com.raghava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootProj03RealTimeDiApplicationTestTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
